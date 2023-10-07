711362d673c9984d58c947c1318cb5e44676a506
Victor Kayk <victorkayk77@gmail.com>
2023-10-07 13:48:56 -0300

    feat: global error handler

---
+++ src/main/java/com/backend/quack/exception/GlobalErrorHandler.java
@@ -0,0 +1,111 @@
+package com.backend.quack.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.AuthenticationException;
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalErrorHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionResponse handleEntityNotFoundException() {
+        return new ExceptionResponse(
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                HttpStatus.NOT_FOUND.value(),
+                List.of(new ErrorType("body", "Entity not found")),
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        List<ErrorType> errors = getErrors(fieldErrors);
+
+        return new ExceptionResponse(
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                HttpStatus.BAD_REQUEST.value(),
+                errors,
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return new ExceptionResponse(
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                HttpStatus.BAD_REQUEST.value(),
+                List.of(new ErrorType("body", ex.getLocalizedMessage())),
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ExceptionResponse handleBadCredentialsException() {
+        return new ExceptionResponse(
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                HttpStatus.UNAUTHORIZED.value(),
+                List.of(new ErrorType("body", "Invalid credentials")),
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ExceptionResponse handleAuthenticationException() {
+        return new ExceptionResponse(
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                HttpStatus.UNAUTHORIZED.value(),
+                List.of(new ErrorType("body", "Authentication failed")),
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ExceptionResponse handleAccessDeniedException() {
+        return new ExceptionResponse(
+                HttpStatus.FORBIDDEN.getReasonPhrase(),
+                HttpStatus.FORBIDDEN.value(),
+                List.of(new ErrorType("body", "Access denied")),
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse handleException(Exception ex) {
+        return new ExceptionResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                List.of(new ErrorType("body", ex.getLocalizedMessage())),
+                LocalDateTime.now()
+        );
+    }
+
+    private static List<ErrorType> getErrors(List<FieldError> fieldErrors) {
+        return fieldErrors
+                .stream()
+                .map(fieldError -> new ErrorType(fieldError.getField(), fieldError.getDefaultMessage()))
+                .toList();
+    }
+
+    public record ErrorType(String field, String message) {
+    }
+
+    public record ExceptionResponse(String message, int status, List<ErrorType> errors, LocalDateTime timestamp) {
+    }
+}
